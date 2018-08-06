438fafe392124691e3f52c740a0b10db7150f784
Unknown <spencer.mcconnell2003@gmail.com>
2018-08-06 15:50:48 -0600

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
+<<<<<<< Updated upstream
 from flask import Flask, render_template, send_from_directory
 #from gpiozero import Button
+=======
+from flask import Flask, render_template, request, redirect
+#import gpiozero
+>>>>>>> Stashed changes
 import os
 
 app = Flask(__name__)
@@ -20,8 +25,13 @@ def main():
 		"title": "Sensor 2",
 		"closed": False
 	}]
+<<<<<<< Updated upstream
 	return render_template('index.html', sensors = sensors)
 	
+=======
+	render_template('index.html', sensors = sensors)
+
+>>>>>>> Stashed changes
 @app.route('/js/<path:path>')
 def js(path):
 	return send_from_directory('js', path)
