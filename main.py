02645092c1f5099424266795ad21449ce7bbc4b9
Luke Bousfield <lukebousfield@Lukes-MacBook-Pro.local>
2018-08-06 15:35:44 -0600

    Added initial template and JavaScript routing, removed unnecessary code

--- main.py
+++ main.py
@@ -1,44 +1,30 @@
 
 from flask import Flask, render_template, request, redirect
-#import gpiozero
+from gpiozero import Button
 import os
 
-
-global status
-global statusval
-global buttonval
-buttonval = "Turn off"
-status = 1
 app = Flask(__name__)
 
 dir = os.getcwd()
 
+global sensors
+sensors = []
+
 @app.route('/', methods=['GET', 'POST'])
 def main():
-	global status, statusval, buttonval
-	if status == 1:
-		statusval = "Status: On"
-
-	else: statusval = "Status: off"
-	return render_template('index.html', status = statusval, tname = buttonval)
-
-
-@app.route('/handle_data', methods=['POST'])
-def handle_data():
-	global status, statusval, buttonval
-	if status == 1:
-		#turn gpio pin on
-		statusval = "Status: Off"
-		status = 0
-		buttonval = "Turn on"
-		return redirect("/", code=302)
-
-	if status == 0:
-		#turn gpio pin off
-		statusval = "Status: On"
-		status = 1
-		buttonval = "Turn off"
-		return redirect("/", code=302)
+	global sensors
+	sensors = [{
+		"title": "Sensor 1",
+		"closed": True
+	}, {
+		"title": "Sensor 2",
+		"closed": False
+	}]
+	render_template('index.html', sensors = sensors)
+	
+@app.route('/js/<path:path>')
+def js(path):
+	return send_from_directory('js', path)
 
 if __name__  == "__main__":
 	app.run(debug=True)
