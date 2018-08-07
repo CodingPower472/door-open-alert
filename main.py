fc0d1fd44195679a6b6b460de04b3cb748ce179d
Unknown <spencer.mcconnell2003@gmail.com>
2018-08-06 20:05:31 -0600

    Push to test on pi

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-
-<<<<<<< Updated upstream
 from flask import Flask, render_template, send_from_directory
-#from gpiozero import Button
-=======
-from flask import Flask, render_template, request, redirect
-#import gpiozero
->>>>>>> Stashed changes
-import os
+from gpiozero import Button, LED
+import os, time
 
 app = Flask(__name__)
 
@@ -14,6 +8,25 @@ dir = os.getcwd()
 
 global sensors
 sensors = []
+
+led = LED(17)
+button = Button(2, hold_time=10)
+button1 = Button(27)
+
+def alertwebsite():
+	print("Alerting the enemy")
+
+def wasttime():
+	print("wasting time")
+	led.on()
+	time.sleep(60)
+	led.off()
+
+
+def setupgpio():
+	button.when_held = wasttime
+	button1.when_pressed = alertwebsite
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -25,16 +38,12 @@ def main():
 		"title": "Sensor 2",
 		"closed": False
 	}]
-<<<<<<< Updated upstream
 	return render_template('index.html', sensors = sensors)
-	
-=======
-	render_template('index.html', sensors = sensors)
 
->>>>>>> Stashed changes
 @app.route('/js/<path:path>')
 def js(path):
 	return send_from_directory('js', path)
 
 if __name__  == "__main__":
+	setupgpio()
 	app.run(debug=True)
